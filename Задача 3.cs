e4b50380a223aa8592b89bc5805fbfc7e40c44c1
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-20 19:53:59 +0300

    Add files via upload

---
+++ Задача 3.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ввидите целое число:");
+            string inputValue = Console.ReadLine();
+            bool isValid = int.TryParse(inputValue, out int number);
+
+            if(lisValid)
+            {
+                Console.WriteLine("Введено некоректное число.");
+                Console.ReadKey();
+                return;
+            }
+
+            if ((number % 5) == 0)
+            {
+                Console.WriteLine($"{number} - четное число.");
+            }
+            else
+            {
+                Console.WriteLine($"{number} не является четным числом.");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
