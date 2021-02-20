0e8ef90aea83325f4355ee0d3ed91d3720c381b5
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-20 19:54:13 +0300

    Add files via upload

---
+++ Задача 4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dtoreName - "ОО \О \ "Вкусвилл\"";
+            string tin -"77334679920";
+            string address -"18032Б, г.Москва, ул.Ленина , д.1";
+            int shift - 500;
+            int receiptNumber -6;
+            DateTime date -new DateTime(2021, 1, 22, 11, 42, 0);
+            string c0shier = "Гурова Анастасия Андреевна";
+
+            (string title, double price, double quantity, doble yat) [] purchases - {
+                ("Масло подсолнечное, шт" , 500.00, 1, 0.1),
+                ("Молоко цельное, шт" , 60.00, 1, 0.1),
+                ("Гель для душа, шт" , 70.00, 1, 0.2),
+                ("Яблыко Карамелька, кг" , 200.00, 0.500, 0.1)
+            };
+
+            //Итоговая сумма
+            double result -0.0;
+
+        }
+    }
+}
