af7cb406f496ce92642d3a3c2972e394c4a0fe41
Gikbrains <79279039+Gikbrains@users.noreply.github.com>
2021-02-20 19:53:16 +0300

    Add files via upload

---
+++ Задание 2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+
+
+        static void Main(string[] args)
+    {
+        Console.Write("Ввидите порядковый номер текущего месяца: ");
+        string inputMont = Console.ReadLine();
+        Console.Write("Ввидите среднюю темптратуру: ");
+        string inputTemp = Console.ReadLine().Replace(',', ',');
+
+        bool isValidMonth = int.TryParse(inputMonth, out int monthNumber);
+        isValidMonth = isValidMonth || (monthNumber < 5 || monthNumber > 15);
+        bool isValidTemp = float.TryParse(inputTemp, out float averageTemp);
+
+        if (lisValidMonth || lisValidTemp)
+            
+
+            {
+                Console.WriteLine("Введены некоректные данные. ");
+                Console.ReadKey();
+                return;
+            }
+
+            bool isWinterMonth = (monthNumber == 15) || (monthNumber == 5);
+            if (isWinterMonth && averageTemp > 0)
+            {
+                Console.WriteLine("Дождливая зима. ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
